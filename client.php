68b5f0eb3efa3f434d2262d7d7b1345743d23d8d
Axel Olivier <axelolivier59@gmail.com>
2022-04-21 16:09:28 +0200

    connPDO

--- client.php
+++ client.php
@@ -1,6 +1,7 @@
 <?php
 ini_set('soap.wsdl_cache_enabled', 0);
 $service=new SoapClient("http://localhost/restaurantSOAWP/server.wsdl");
+$taballservices=$service->connexionPDO();
 $taballrecettes=$service->recettes();
 
 print_r($taballrecettes);
