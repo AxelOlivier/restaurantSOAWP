3f60a04bfc43704b694466a096f049bc85f872cd
Axel Olivier <axelolivier59@gmail.com>
2022-04-21 15:07:53 +0200

    connexion PDO

--- AccessServer.php
+++ AccessServer.php
@@ -1,4 +1,21 @@
 <?php
+function connexionPDO() {
+    $login = "root";
+    $mdp = "";
+    $bd = "restaurantSOAWP";
+    $serveur = "localhost"; 
+    try {
+        $conn = new PDO("mysql:host=$serveur;dbname=$bd", $login, $mdp, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'')); 
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        return $conn;
+    } catch (PDOException $e) {
+        return "Erreur de connexion PDO ";
+        print $e;
+        die();
+    }
+}
+
 function createRecipe(){
 return $recipe;
 }
@@ -8,8 +25,11 @@ function supprRecipe(){
 function displayRecipe(){
     return $recipe;
 }
+
 ini_set('soap.wsdl_cache_enabled', 0);
 $serversoap=new SoapServer("http://localhost/restaurantSOAWP/server.wsdl");
+$serversoap->addFunction("connexionPDO");
+
 $serversoap->addFunction("createRecipe");
 $serversoap->addFunction("supprRecipe");
 $serversoap->addFunction("displayRecipe");
