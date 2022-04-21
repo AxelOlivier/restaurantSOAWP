b70b0461fe4e50ae15b1fa2015b1d66ffde6badc
Axel Olivier <axelolivier59@gmail.com>
2022-04-21 16:23:26 +0200

    BDD up

--- bdd.sql
+++ bdd.sql
@@ -15,8 +15,27 @@ CREATE TABLE IF NOT EXISTS `Ingredient`
  ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 INSERT INTO `Ingredients` (`id`, `nomIngredient`, `recetteID`) VALUES
-(1, 'tranche de pain', 2),
+(1, 'tranche de pain', 1),
 (2, 'cheddar', 1),
 (3, 'oeuf', 1),
 (4, 'jambon', 1),
 (5, 'moutarde', 1),
+
+DROP TABLE IF EXISTS `Recette`;
+CREATE TABLE IF NOT EXISTS `Recette` (
+  `id` int(1) NOT NULL AUTO_INCREMENT,
+  `nomRecette` varchar(100) NOT NULL,
+  `descriptionRecette` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+ALTER TABLE `Ingredient`
+  ADD CONSTRAINT `ingredient_ibfk_1` 
+    FOREIGN KEY (`idRecette`) 
+      REFERENCES `Recette` (`id`);
+COMMIT;
+
+INSERT INTO `Recette` (`id`, `nomRecette`, `descriptionRecette`) 
+    VALUES
+      (1, 'Un bon ptit Welsh', 
+            'Tout es fait maison même la moutartde!!');
