f42c53447f510b97471174337bdb94675f6e7672
Axel Olivier <axelolivier59@gmail.com>
2022-04-21 13:51:06 +0200

    add bdd & client

---
+++ client.php
@@ -0,0 +1,7 @@
+<?php
+ini_set('soap.wsdl_cache_enabled', 0);
+$service=new SoapClient("http://localhost/restaurantSOAWP/server.wsdl");
+$taballrecettes=$service->recettes();
+
+print_r($taballrecettes);
+?>
